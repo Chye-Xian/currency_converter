1e565f3eca94448b4dbc8929a1991cc061af128b
Chye-Xian <chye-xian@pocketsmith.com>
2015-04-24 09:48:27 +1200

    js injection re-jig

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -1,13 +1,15 @@
 class ConvertsController < ApplicationController
-  #require 'rubygems'
   #require 'exchange'
   
+  protect_from_forgery :except => :create
+  before_filter :set_response_header
+  
   def index
-    render :layout => 'widget.js.erb'
+    render :layout => 'widget.js.erb', :content_type => 'application/javascript'
   end
   
   def show
-    render :layout => 'widget.js.erb'
+    render :layout => 'widget.js.erb', :content_type => 'application/javascript'
   end
   
   def create
@@ -16,6 +18,11 @@ class ConvertsController < ApplicationController
     @date = params[:date].split("-")
     @result = @amount.in(params[:fromCurr].to_sym, at: Time.gm(@date[0],@date[1],@date[2]))
               .to(params[:toCurr].to_sym).to_f
-    render :layout => 'widget.js.erb'
+              
+    render :layout => 'widget.js.erb', :content_type => 'application/javascript'
   end
+  
+  def set_response_header
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
 end
